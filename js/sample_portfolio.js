4417dffc05730a6d9d6138f56e84211856b52102
rayanm2acha <rayanmaacha@gmail.com>
2023-04-13 11:55:14 -0700

    javascript new code

--- js/sample_portfolio.js
+++ js/sample_portfolio.js
@@ -1,38 +1,43 @@
 // These functions open and close the contact form
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
+// Slideshow variables
+  var slideIndex = 1;
+  showSlides(slideIndex);
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+  // Next/previous controls
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
 
-// This function displays the first image in the slideshow when the page loads
-var slideIndex = 1;
-showSlides(slideIndex);
+  // Thumbnail image controls
+  function currentSlide(n) {
+      showSlides(slideIndex = n);
+  }
 
-// This function changes the slide when the left or right arrows are clicked
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+  function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {
+          slideIndex = 1;
+      }
+      if (n < 1) {
+          slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+  }
 
-// This function changes the slide when the dots are clicked
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+  // Contact form functions
+  function openForm() {
+      document.getElementById("myForm").style.display = "block";
+  }
 
-
-function showSlides(n) {
-    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
-    var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
-    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
-    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
-    }
-    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
-    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
-}
+  function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+  }
